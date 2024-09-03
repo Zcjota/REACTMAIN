c21629566ae8b074c29c86f54a8e6fdbc185cad6
Jr <belzekert@gmail.com>
2024-09-03 14:48:41 -0400

    Update Modelo3D_Universidad.js

--- src/Materiales/Modelo3D_Universidad.js
+++ src/Materiales/Modelo3D_Universidad.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useAnimations } from '@react-three/drei'
 
 export function Modelo3D_Universidad(props) {
   const group = useRef();
